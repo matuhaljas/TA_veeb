ba14ca73b157b350928334ac6e13223ab9a58579
Marcus Haljasoks <matuhaljas@users.noreply.github.com>
2025-09-29 13:33:43 +0300

    tegime veebi

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const fs = require("fs");
+//päringu lahtiharutaja POST jaoks
+const bodyparser = require("body-parser");
+const dateEt = require("./src/dateTimeET");
+const textRef = "public/txt/vanasonad.txt";
+
+//käivitan express.js funktsiooni ja annan nime app
+const app = express();
+
+//määran veebilehe renderdamise mootori
+app.set("view engine", "ejs");
+//määran ühe päris kataloogi avalikult kättesaadavaks
+app.use(express.static("public"));
+//parsime päringu URL-i, lipp false, kui ainult tekst ja true, kui muid andmeid ka
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.get("/", (req, res)=>{
+    res.render("index");
+});
+
+app.get("/timenow", (req, res)=>{
+    const weekDayNow = dateEt.weekDay();
+    const dateNow = dateEt.fullDate();
+    res.render("timenow", {weekDayNow: weekDayNow, dateNow: dateNow})
+});
+
+app.get("/vanasonad", (req, res)=>{
+    let folkWisdom = [];
+    fs.readFile(textRef, "utf-8", (err, data) => {
+        if(err) {
+            //kui tuleb error siis valjastame lehe aga lihtsalt vanasõnu pole
+            res.render("genericlist", {heading: "Valik Eesti vanasõnu", listData: ["Ei leidnud ühtegi vanasõna"]});
+        }
+        else {
+            folkWisdom = data.split(";");
+            res.render("genericlist", {heading: "Valik Eesti vanasõnu", listData: folkWisdom});
+        }
+    });
+});
+
+app.get("/regvisit", (req, res)=>{
+	res.render("reqvisit");
+});
+
+app.post("/regvisit", (req, res)=> {
+    console.log(req.body);
+    //avan teksti faili kirjutamiseks sellisel moel, et kui teda pole, luuakse
+    //parameeter "a"
+    fs.open("public/txt/visitlog.txt", "a", (err, file)=>{
+        if (err) {
+            throw(err);
+        }
+        else {
+            //faili senisele sisule lisamine
+            fs.appendFile("public/txt/visitlog.txt", req.body.nameInput + "; ", (err)=> {
+                if (err) {
+                    throw(err);
+                }
+                else {
+                    console.log("Salvestatud!")
+                    res.render("reqvisit");
+                }
+            });
+        }
+    });
+});
+
+app.listen(5122);
---
+++ src/dateTimeET.js
@@ -0,0 +1,21 @@
+//function dateNowFormatedET() {
+const dateNowFormatedET = function() {
+    let timeNow = new Date();
+    const monthNamesET = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+    const dayNamesET = ["pühapäev", "esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev"]
+    return timeNow.getDate() + "." + monthNamesET[timeNow.getMonth()] + "." + timeNow.getFullYear();
+}
+
+const weekDay = function() {
+    let timeNow = new Date();
+    const dayNamesET = ["pühapäev", "esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev"]
+    return dayNamesET[timeNow.getDay()]
+}
+
+const timeNowFormatedET = function() {
+    let timeNow = new Date();
+    return timeNow.getHours() + ":" + timeNow.getMinutes()
+}
+
+//ekspordin kõik vajalikud
+module.exports = {weekDay, fullDate: dateNowFormatedET, fullTime: timeNowFormatedET}
